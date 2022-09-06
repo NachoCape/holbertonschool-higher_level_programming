72b0d2b535c63e8c6d541e2a3e311b6faa0103f8
NachoCape <igcapezzolo@hotmail.com>
2022-09-06 10:34:09 -0700

    add task5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i < 10:
-        print("0", end="")
-    if i <= 98:
-        print(f"{i}", end=", ")
-    else:
+    if i == 99:
         print(f"{i}")
+    print("{:02d}".format(i), end=", ")
