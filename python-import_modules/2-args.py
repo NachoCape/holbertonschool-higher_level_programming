4aaebef64a24b5b08f3482a91212dcadba702ccf
NachoCape <igcapezzolo@hotmail.com>
2022-09-09 12:10:18 -0700

    task2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
         print("0 arguments.")
     elif len_of_argv == 1:
         print("1 argument:")
-        print("1: {:d}".format(sys.argv[1]))
+        print("1: {}".format(sys.argv[1]))
     else:
         print("{} arguments:".format(len_of_argv))
         for i in range(len_of_argv):
