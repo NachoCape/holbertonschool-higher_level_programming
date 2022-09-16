015af570dc8192f077b750543d32b56caf2b94fb
Ignacio Capezzolo <5139@holbertonstudents.com>
2022-09-16 13:02:03 -0300

    add task2

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    cont = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            cont += 1
+        except (TypeError, ValueError):
+            continue
+    print(f"")
+    return cont
