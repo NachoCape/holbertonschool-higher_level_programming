495ff00f1b0e0f6cc337fe6cef19543caf3ab57a
Ignacio Capezzolo <5139@holbertonstudents.com>
2022-09-16 12:05:30 -0300

    add task0

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+def safe_print_list(my_list=[], x=0):
+    cont = 0
+    try:
+        for i in my_list:
+            if cont < x:
+                print(f"{i}", end="")
+                cont += 1
+        print(f"")
+    except Exception:
+        pass
+    return cont
