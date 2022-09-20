2b6a2b7b3416983d276db863c8aba20710944763
NachoCape <igcapezzolo@hotmail.com>
2022-09-19 22:17:13 -0300

    add task3

---
+++ python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square"""
+
+
+from ctypes import sizeof
+
+
+class Square:
+    """class Square"""
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Public instance area"""
+        return self.__size ** 2
