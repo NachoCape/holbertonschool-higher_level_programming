59d8a3b65a20f7887e97c996724f52039630d6d1
NachoCape <igcapezzolo@hotmail.com>
2022-09-21 20:03:16 -0300

    add task2 files

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def matrix_divided(matrix, div):
+    if type(div) is not int and type(div) is not float:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    if not matrix:
+        raise TypeError("matrix must be a matrix \
+            (list of lists) of integers/floats")
+    mtx = matrix
+    new_matrix = []
+    lenght = len(mtx[0])
+    for i in range(0, len(mtx)):
+        new_matrix.append([])
+        for j in range(0, len(mtx[i])):
+            if len(mtx[i]) != lenght:
+                raise TypeError("Each row of the matrix\
+                    must have the same size")
+            if type(mtx[i][j]) is not int and type(mtx[i][j]) is not float:
+                raise TypeError("matrix must be a matrix \
+                    (list of lists) of integers/floats")
+            new_matrix[i].append(round(mtx[i][j] / div, 2))
+    return new_matrix
