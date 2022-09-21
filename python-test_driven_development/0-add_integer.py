263a696923ccd6f4df305a2b2cf2a37cd68553e9
NachoCape <igcapezzolo@hotmail.com>
2022-09-21 19:00:04 -0300

    update test

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -8,7 +8,7 @@
         """
 
 
-def add_integer(a, b=98):
+def add_integer(a, b):
     """ Doc """
     if a is None or (type(a) is not int and type(a) is not float):
         raise TypeError("a must be an integer")
