5a0d5136903a9a6f20918755f02a6fef7da0b5d8
Ignacio Capezzolo <5139@holbertonstudents.com>
2022-09-21 14:31:55 -0300

    update test

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,15 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
+"""Write a function that adds 2 integers
+    a and b must be integers or floats, otherwise raise a TypeError
+    exception with the message a must be an integer or b must be an integer
+    a and b must be first casted to integers if they are float
+    Returns an integer: the addition of a and b
+    You are not allowed to import any module
+        """
+
+
 def add_integer(a, b=98):
+    """add 2 integers"""
     if isinstance(a, int) or isinstance(a, float):
         a = round(a)
     else:
