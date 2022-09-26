82a8573d9c696d32d202772f1981b2e4dad9515d
NachoCape <igcapezzolo@hotmail.com>
2022-09-26 13:47:14 -0300

    add readme and task0

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Lookup module"""
+
+
+def lookup(obj):
+    """Write a function that returns the list of available
+    attributes and methods of an object:"""
+
+    return dir(obj)
