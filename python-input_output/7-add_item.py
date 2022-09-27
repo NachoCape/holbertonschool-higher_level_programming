3712848a37e2a8fb0f0c28fb89c244ba420ccdb6
NachoCape <igcapezzolo@hotmail.com>
2022-09-27 19:25:01 -0300

    change documentation task7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Load, add, save Module"""
+"""Module"""
 
 
 import sys
@@ -11,6 +11,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 list = []
 filename = "add_item.json"
+
 list = load_from_json_file(filename)
 for i in range(1, len(sys.argv) - 1):
-    list = save_to_json_file(i, filename)
+    list = save_to_json_file(sys.argv[i], filename)
