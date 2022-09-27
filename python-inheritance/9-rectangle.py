174d873dc9406b7ad4f597f651e1a6323ab15841
NachoCape <igcapezzolo@hotmail.com>
2022-09-26 22:07:39 -0300

    add task9

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Rectangle Module"""
+
+
+from turtle import width
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Create a Rectangle"""
+    def __init__(self, width, height):
+        BaseGeometry.integer_validator(Rectangle, "width", width)
+        BaseGeometry.integer_validator(Rectangle, "height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__height * self.__width
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
