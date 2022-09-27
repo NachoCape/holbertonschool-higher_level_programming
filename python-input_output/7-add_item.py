a741e1b33c1a54e79479e13479e7ea116a331825
NachoCape <igcapezzolo@hotmail.com>
2022-09-27 19:20:42 -0300

    update docu task7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""
-    Load, add, save Module
-    adds all arguments to a Python list, and then save them to a file
-"""
+"""Load, add, save Module"""
 
 
 import sys
+
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
 
 list = []
 filename = "add_item.json"
