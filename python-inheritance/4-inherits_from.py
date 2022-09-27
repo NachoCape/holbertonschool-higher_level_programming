75b104ab9389a7ab629a4343ea7efded2a523763
NachoCape <igcapezzolo@hotmail.com>
2022-09-26 21:20:15 -0300

    add task4 and task7 files

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""returns True if the object is an instance of a class that inherited
+(directly or indirectly) from the specified class ; otherwise False"""
+
+
+def inherits_from(obj, a_class):
+    """Function that will check"""
+    if type(obj) is a_class:
+        return False
+    return isinstance(obj, a_class)
