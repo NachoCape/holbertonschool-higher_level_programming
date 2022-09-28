86adc4333c3844839204ff3fbfd1d7accdb37988
NachoCape <igcapezzolo@hotmail.com>
2022-09-28 13:48:15 -0300

    add task7 fix

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,8 +9,13 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
-list = []
-list = load_from_json_file(filename)
-for i in range(1, len(sys.argv) - 1):
-    list.append(sys.argv[i])
-list = save_to_json_file(list, filename)
+try:
+    list = load_from_json_file(filename)
+    for i in range(1, len(sys.argv)):
+        list.append(sys.argv[i])
+    save_to_json_file(list, filename)
+except Exception:
+    list = []
+    for i in range(1, len(sys.argv)):
+        list.append(sys.argv[i])
+    save_to_json_file(list, filename)
