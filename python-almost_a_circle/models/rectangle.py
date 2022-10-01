87ea9fe3d17dac0a06656492b8ce261b241a2c6f
NachoCape <igcapezzolo@hotmail.com>
2022-10-01 15:10:08 -0300

    add task8

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -78,3 +78,16 @@ class Rectangle(Base):
         """prints the rectangle with his different attributes"""
         return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - \
 {self.__width}/{self.__height}"
+
+    def update(self, *args):
+        """assigns an argument to each attribute"""
+        if args[0]:
+            self.__id = args[0]
+        if len(args) > 1:
+            self.__width = args[1]
+        if len(args) > 2:
+            self.__height = args[2]
+        if len(args) > 3:
+            self.__x = args[3]
+        if len(args) > 4:
+            self.__y = args[4]
