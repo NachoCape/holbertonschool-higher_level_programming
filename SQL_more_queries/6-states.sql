559824ab8f3b3748193b265d76a66b1b0b7cb98c
NachoCape <igcapezzolo@hotmail.com>
2022-11-05 12:16:35 -0300

    fix if table already exists command USE, task6

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -2,7 +2,8 @@
 --      states description:
 --          id INT unique, auto generated, can’t be null and is a primary key
 --          name VARCHAR(256) can’t be null
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa; 
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(256),
