dec840554043852040d2a0f0e2829ec7e00ea44f
NachoCape <igcapezzolo@hotmail.com>
2022-11-08 10:04:31 -0800

    fix type mistake task2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     db = connect(host="localhost", port=3306, user=_user,
                  password=_pass, db=_db)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE statesname='{}'\
+    cur.execute("SELECT * FROM states WHERE states.name='{}'\
             ORDER BY states.id ASC".format(_name_state))
     rows = cur.fetchall()
     for row in rows:
