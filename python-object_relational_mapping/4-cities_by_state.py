31230c9ac0b012a699b2a6ea7a13353c2716c1f4
NachoCape <igcapezzolo@hotmail.com>
2022-11-09 08:30:31 -0800

    add task4

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    takes in an argument and displays all values in the states table
+    of hbtn_0e_0_usa where name matches the argument
+"""
+from MySQLdb import connect
+import sys
+
+
+if __name__ == "__main__":
+    _user = sys.argv[1]
+    _pass = sys.argv[2]
+    _db = sys.argv[3]
+
+    db = connect(host="localhost", port=3306, user=_user,
+                 password=_pass, db=_db)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM cities ORDER BY cities.id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
