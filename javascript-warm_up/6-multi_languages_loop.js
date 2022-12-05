0039ad06a394be2aec34dd53e5d301ed75e300f8
NachoCape <igcapezzolo@hotmail.com>
2022-12-05 23:13:40 +0000

    implement task6 with a for

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
 const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-arr.forEach((item) => {
-  console.log(item);
-});
+for (let i = 0; arr[i]; i++) {
+  console.log(arr[i]);
+}
