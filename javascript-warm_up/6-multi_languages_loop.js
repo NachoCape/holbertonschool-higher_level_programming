fbd140b5e57f8466a41d09b409a288d3e8ecb586
NachoCape <igcapezzolo@hotmail.com>
2022-12-05 23:16:56 +0000

    ?

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
 const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; arr[i]; i++) {
-  console.log(arr[i]);
-}
+arr.forEach(function(item) {
+  console.log(item);
+});
