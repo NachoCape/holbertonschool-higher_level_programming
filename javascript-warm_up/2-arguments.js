c166875936a44bc2f190ebded31a5c45f10f09a0
NachoCape <igcapezzolo@hotmail.com>
2022-12-05 22:15:31 +0000

    fix task2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,7 +2,4 @@
 
 import { argv } from 'process';
 
-// print process.argv
-argv.forEach((val, index) => {
-  console.log(`${index}: ${val}`);
-});
+if (argv.length === 0) { console.log('No argument'); } else if (argv.length === 1) { console.log('Argument found'); } else { console.log('Arguments found'); }
