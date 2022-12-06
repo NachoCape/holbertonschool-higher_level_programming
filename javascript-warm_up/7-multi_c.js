9fdca48f8767b7c1fe4f87d40783d46a8af15e80
NachoCape <igcapezzolo@hotmail.com>
2022-12-06 01:44:56 +0000

    fix only use 2 console.log task7

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
 
 const argv = process.argv;
-
-if (argv.length === 2) {
+if (isNaN(argv[2])) {
   console.log('Missing number of occurrences');
-} else if (isNaN(argv[2])) {
-  console.log('');
 } else {
   for (let i = 0; i < argv[2]; i++) {
     console.log('C is fun');
