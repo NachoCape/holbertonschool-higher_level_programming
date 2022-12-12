47c3a5eabc0ae346ff8c34fab8cf302ccd5378fd
NachoCape <igcapezzolo@hotmail.com>
2022-12-12 13:57:07 +0000

    remove a space in the output, fix task2

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -3,9 +3,9 @@
 const argv = process.argv;
 const request = require('request');
 
-request(argv[2], function (err, res) {
+request(argv[2], function (err, res, body) {
   if (err) {
     console.log(err);
   }
-  console.log('code: ', res && res.statusCode);
+  console.log('code:', res && res.statusCode);
 });
